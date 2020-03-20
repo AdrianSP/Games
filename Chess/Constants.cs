951a31eee416feeefe85707c90182e58b5ab7b8f
Adrian Serrano <adrianserrano15@gmail.com>
2020-03-20 18:28:44 -0400

    Added Chess

---
+++ Chess/Constants.cs
@@ -0,0 +1,20 @@
+﻿public enum PieceName
+{
+    Pawn,
+    Rook,
+    Knight,
+    Bishop,
+    Queen,
+    King
+}
+
+public enum Team
+{
+    White,
+    Black
+}
+public enum BoardMode
+{
+    Selection,
+    Movement
+}
